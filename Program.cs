b7f2bbf6e2d1fa7dd62f19c542c5ec91bcc5db6e
PedroHenriqueSI <57466217+PedroHenriqueSI@users.noreply.github.com>
2020-09-21 17:04:05 -0300

    Trabalhando com UPcasting

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-namespace Herança
+namespace Herança.Entities
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Account acc = new Account(2001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(512, "Araguaína", 0.0, 500.0);
 
+
+            //Trabalhando com UPcasting
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(513, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(552, "Ana", 0.0, 0.02);
+
+            
         }
     }
 }
